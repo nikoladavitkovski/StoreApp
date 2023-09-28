a2ddb4fff372ea3874ec7aa715ac3b20aa12b542
Nikola Davitkovski <82374008+nikoladavitkovski@users.noreply.github.com>
2023-09-28 15:35:58 +0200

    Add files via upload

---
+++ Houses.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GadgetApp.Classes
+{
+    public class Houses
+    {
+        public int numberofflats;
+
+        public bool primaryschoolnear;
+
+        public Houses(int numberofflats, bool primaryschoolnear)
+        {
+            this.numberofflats = numberofflats;
+            this.primaryschoolnear = primaryschoolnear;
+        }
+
+        public string FullArea(float price, int square)
+        {
+            if (price * square == 3600)
+            {
+                Console.WriteLine($"The maximum will be {square.GetType()}");
+            }
+
+            if (primaryschoolnear)
+            {
+                Console.WriteLine("The price is 3% higher");
+            }
+            return price.ToString();
+        }
+    }
+}
