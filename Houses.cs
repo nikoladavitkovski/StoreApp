d104350859db6a8d55a00aace7d1a580f8aac955
Nikola Davitkovski <82374008+nikoladavitkovski@users.noreply.github.com>
2023-09-29 18:42:45 +0200

    Update Houses.cs

--- Houses.cs
+++ Houses.cs
@@ -9,10 +9,20 @@ namespace GadgetApp.Classes
     public class Houses
     {
         public float price;
+
+        public List<DateTime> dates = new List<DateTime>()
+        {
+            new DateTime(2022,12,07)
+        };
         
         public int numberofflats;
 
         public bool primaryschoolnear;
+
+        public void GetHouse()
+        {
+            Console.WriteLine(price);
+        }
 
         public Houses(float price,int numberofflats, bool primaryschoolnear)
         {
